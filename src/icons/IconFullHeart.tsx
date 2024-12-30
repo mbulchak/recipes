30e380465082d1c11e46063a36743b1adeaf9f57
Mariia Bulchak <bulchakmariiast@gmail.com>
2024-12-30 19:24:08 +0200

    add components icons

---
+++ src/icons/IconFullHeart.tsx
@@ -0,0 +1,9 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+export const IconFullHeart = () => {
+  return (
+    <span className="icon">
+      <i className="fa-solid fa-heart"></i>
+    </span>
+  );
+};
---
+++ src/icons/IconHeart.tsx
@@ -0,0 +1,11 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+export const IconHeart = () => {
+  return (
+    // <div className=''>
+    <span className="icon">
+      <i className="fa-regular fa-heart"></i>
+    </span>
+    // </div>
+  );
+};
